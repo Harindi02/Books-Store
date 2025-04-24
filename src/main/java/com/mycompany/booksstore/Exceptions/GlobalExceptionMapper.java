e733e55c5fbc2fa614fb15ab6810fe50833eb8e2
harindi02 <durangaharindi@gmail.com>
2025-04-25 00:31:13 +0530

    Added Mapper class

--- src/main/java/com/mycompany/booksstore/Exceptions/GlobalExceptionMapper.java
+++ src/main/java/com/mycompany/booksstore/Exceptions/GlobalExceptionMapper.java
@@ -8,6 +8,49 @@ package com.mycompany.booksstore.Exceptions;
  *
  * @author ASUS OLED
  */
-public class GlobalExceptionMapper {
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+    @Override
+    public Response toResponse(Exception exception) {
+        if (exception instanceof BookNotFoundException) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErrorMessage(exception.getMessage()))
+                    .build();
+        } else if (exception instanceof AuthorNotFoundException || 
+                  exception instanceof CustomerNotFoundException ||
+                  exception instanceof CartNotFoundException) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErrorMessage(exception.getMessage()))
+                    .build();
+        } else if (exception instanceof InvalidInputException) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(new ErrorMessage(exception.getMessage()))
+                    .build();
+        } else if (exception instanceof OutofStockFoundException) {
+            return Response.status(Response.Status.CONFLICT)
+                    .entity(new ErrorMessage(exception.getMessage()))
+                    .build();
+        } else {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new ErrorMessage("Internal server error"))
+                    .build();
+        }
+    }
+}
+
+class ErrorMessage {
+    private String message;
     
+    public ErrorMessage(String message) {
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
 }
