e7b997d7052d88f40b8722d7dbb8f2bb8e7fb57e
harindi02 <durangaharindi@gmail.com>
2025-04-25 01:27:56 +0530

    feat: updated the App Configuration file

--- src/main/java/com/mycompany/booksstore/JAXRSConfiguration.java
+++ src/main/java/com/mycompany/booksstore/JAXRSConfiguration.java
@@ -1,5 +1,14 @@
 package com.mycompany.booksstore;
 
+
+import com.mycompany.booksstore.Exceptions.GlobalExceptionMapper;
+import com.mycompany.booksstore.resources.AuthorResource;
+import com.mycompany.booksstore.resources.BookResource;
+import com.mycompany.booksstore.resources.CartResource;
+import com.mycompany.booksstore.resources.CustomerResource;
+import com.mycompany.booksstore.resources.OrderResource;
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -7,7 +16,19 @@ import javax.ws.rs.core.Application;
  * Configures JAX-RS for the application.
  * @author Juneau
  */
-@ApplicationPath("resources")
+ 
+
+@ApplicationPath("/api")
 public class JAXRSConfiguration extends Application {
-    
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(BookResource.class);
+        resources.add(AuthorResource.class);
+        resources.add(CustomerResource.class);
+        resources.add(CartResource.class);
+        resources.add(OrderResource.class);
+        resources.add(GlobalExceptionMapper.class);
+        return resources;
+    }
 }
