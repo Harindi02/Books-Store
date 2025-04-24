84d6836b2a44f5246c810d52aacd904d38e4d835
harindi02 <durangaharindi@gmail.com>
2025-04-25 01:14:43 +0530

    feat:created the bookResource class

--- src/main/java/com/mycompany/booksstore/resources/BookResource.java
+++ src/main/java/com/mycompany/booksstore/resources/BookResource.java
@@ -8,6 +8,48 @@ package com.mycompany.booksstore.resources;
  *
  * @author ASUS OLED
  */
+ 
+
+import com.mycompany.booksstore.Service.BookService;
+import com.mycompany.booksstore.models.Book;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/books")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class BookResource {
-    
+    private BookService bookService = new BookService();
+
+    @GET
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @POST
+    public Response addBook(Book book) {
+        Book newBook = bookService.addBook(book);
+        return Response.status(Response.Status.CREATED).entity(newBook).build();
+    }
+
+    @GET
+    @Path("/{isbn}")
+    public Book getBook(@PathParam("isbn") String isbn) {
+        return bookService.getBook(isbn);
+    }
+
+    @PUT
+    @Path("/{isbn}")
+    public Book updateBook(@PathParam("isbn") String isbn, Book book) {
+        return bookService.updateBook(isbn, book);
+    }
+
+    @DELETE
+    @Path("/{isbn}")
+    public Response deleteBook(@PathParam("isbn") String isbn) {
+        bookService.deleteBook(isbn);
+        return Response.noContent().build();
+    }
 }
